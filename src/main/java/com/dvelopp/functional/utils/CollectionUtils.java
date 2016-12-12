13c605ee608ab376f5a8c7c53e4f3cd9c73d944a
dvelopp <dvelopp@gmail.com>
2016-12-12 23:04:57 +0200

    CollectionUtils clean up part 1

--- src/main/java/com/dvelopp/functional/utils/CollectionUtils.java
+++ src/main/java/com/dvelopp/functional/utils/CollectionUtils.java
@@ -38,13 +38,13 @@ public final class CollectionUtils {
     }
 
     /**
-     * Map collection to a new list according to the mapper
+     * Returns a list consisting of the results of applying the given function to the elements of the given collection.
      *
-     * @param collection - source collection
-     * @param mapper     - mapper that describes how to map each element of the collection
-     * @param <T>        - source collection elements type
-     * @param <R>        - target list elements type
-     * @return mapped list
+     * @param collection The source collection.
+     * @param mapper     The function to apply to each element.
+     * @param <T>        The source collection elements type.
+     * @param <R>        The target list elements type.
+     * @return the new list that contains result of applying the function for elements of given collection.
      */
     public static <T, R> List<R> mapToList(Collection<T> collection, Function<? super T, ? extends R> mapper) {
         requireNonNull(collection, mapper);
@@ -52,15 +52,18 @@ public final class CollectionUtils {
     }
 
     /**
-     * Map collection to a new list according to the mapper using {@link BiFunction} and additional argument
+     * Returns a list consisting of the results of applying the given function to the elements of the given collection.
+     * Consumes additional argument and requires BiFunction. It can be useful when there is invocation of the method
+     * with two arguments or just method with one argument, but on instance variable. It helps to write a lambda or
+     * method reference cleaner in that case.
      *
-     * @param collection - source collection
-     * @param mapper     - mapper that describes how to map each element of the collection
-     * @param arg        - second argument for bi function
-     * @param <T>        - source collection elements type
-     * @param <R>        - target list elements type
-     * @param <S>        - argument type
-     * @return mapped list
+     * @param collection The source collection.
+     * @param mapper     The function to apply to each element.
+     * @param arg        The second argument that is used for {@link BiFunction}.
+     * @param <T>        The source collection elements type.
+     * @param <R>        The target list elements type.
+     * @param <S>        The second argument type.
+     * @return the new list that contains result of applying the function for elements of given collection.
      */
     public static <T, R, S> List<R> mapToList(Collection<T> collection,
                                               BiFunction<? super T, ? super S, ? extends R> mapper, S arg) {
@@ -69,13 +72,13 @@ public final class CollectionUtils {
     }
 
     /**
-     * Map collection to a new set according to the mapper
+     * Returns a set consisting of the results of applying the given function to the elements of the given collection.
      *
-     * @param collection - source collection
-     * @param mapper     - mapper that describes how to map each element of the collection
-     * @param <T>        - source collection elements type
-     * @param <R>        - target set elements type
-     * @return mapped set
+     * @param collection The source collection.
+     * @param mapper     The function to apply to each element.
+     * @param <T>        The source collection elements type.
+     * @param <R>        The target set elements type.
+     * @return the new set that contains result of applying the function for elements of given collection.
      */
     public static <T, R> Set<R> mapToSet(Collection<T> collection, Function<? super T, ? extends R> mapper) {
         requireNonNull(collection, mapper);
@@ -83,15 +86,18 @@ public final class CollectionUtils {
     }
 
     /**
-     * Map collection to a new set according to the mapper using {@link BiFunction} and additional argument
+     * Returns a set consisting of the results of applying the given function to the elements of the given collection.
+     * Consumes additional argument and requires BiFunction. It can be useful when there is invocation of the method
+     * with two arguments or just method with one argument, but on instance variable. It helps to write a lambda or
+     * method reference cleaner in that case.
      *
-     * @param collection - source collection
-     * @param mapper     - mapper that describes how to map each element of the collection
-     * @param arg        - second argument for bi function
-     * @param <T>        - source collection elements type
-     * @param <R>        - target set elements type
-     * @param <S>        - argument type
-     * @return mapped set
+     * @param collection The source collection.
+     * @param mapper     The function to apply to each element.
+     * @param arg        The second argument that is used for {@link BiFunction}.
+     * @param <T>        The source collection elements type.
+     * @param <R>        The target set elements type.
+     * @param <S>        The second argument type.
+     * @return the new set that contains result of applying the function for elements of given collection.
      */
     public static <T, R, S> Set<R> mapToSet(Collection<T> collection,
                                             BiFunction<? super T, ? super S, ? extends R> mapper, S arg) {
@@ -100,13 +106,14 @@ public final class CollectionUtils {
     }
 
     /**
-     * Map list to a new list according to the mapper
+     * Returns a list consisting of the results of applying the given function to the elements of the given list.
+     * Short form of invocation.
      *
-     * @param list   - source list
-     * @param mapper - mapper that describes how to map each element of the collection
-     * @param <T>    - source list elements type
-     * @param <R>    - target list elements type
-     * @return mapped list
+     * @param list   The source collection.
+     * @param mapper The function to apply to each element.
+     * @param <T>    The source collection elements type.
+     * @param <R>    The target list elements type.
+     * @return the new list that contains result of applying the function for elements of given collection.
      */
     public static <T, R> List<R> map(List<T> list, Function<? super T, ? extends R> mapper) {
         requireNonNull(list, mapper);
@@ -114,13 +121,14 @@ public final class CollectionUtils {
     }
 
     /**
-     * Map set to a new set according to the mapper
+     * Returns a set consisting of the results of applying the given function to the elements of the given set.
+     * Short form of invocation.
      *
-     * @param set    - source set
-     * @param mapper - mapper that describes how to map each element of the collection
-     * @param <T>    - source set elements type
-     * @param <R>    - target set elements type
-     * @return mapped set
+     * @param set    The source collection.
+     * @param mapper The function to apply to each element.
+     * @param <T>    The source collection elements type.
+     * @param <R>    The target set elements type.
+     * @return the new set that contains result of applying the function for elements of given collection.
      */
     public static <T, R> Set<R> map(Set<T> set, Function<? super T, ? extends R> mapper) {
         requireNonNull(set, mapper);
@@ -128,45 +136,52 @@ public final class CollectionUtils {
     }
 
     /**
-     * Map list to a new list according to the mapper using {@link BiFunction} and additional argument
+     * Returns a list consisting of the results of applying the given function to the elements of the given list.
+     * Consumes additional argument and requires BiFunction. It can be useful when there is invocation of the method
+     * with two arguments or just method with one argument, but on instance variable. It helps to write a lambda or
+     * method reference cleaner in that case.
      *
-     * @param list   - source list
-     * @param mapper - BiFunction mapper that describes how to map each element of the collection
-     * @param arg    - second argument for bi function
-     * @param <T>    - source list elements type
-     * @param <R>    - target list elements type
-     * @param <S>    - argument type
-     * @return mapped list
+     * @param list   The source collection.
+     * @param mapper The function to apply to each element.
+     * @param arg    The second argument that is used for {@link BiFunction}.
+     * @param <T>    The source collection elements type.
+     * @param <R>    The target list elements type.
+     * @param <S>    The second argument type.
+     * @return the new list that contains result of applying the function for elements of given list.
      */
     public static <T, R, S> List<R> map(List<T> list, BiFunction<? super T, ? super S, ? extends R> mapper, S arg) {
         return map(list, function(mapper, arg));
     }
 
     /**
-     * Map set to a new set according to the mapper using {@link BiFunction} and additional argument
+     * Returns a set consisting of the results of applying the given function to the elements of the given set.
+     * Consumes additional argument and requires BiFunction. It can be useful when there is invocation of the method
+     * with two arguments or just method with one argument, but on instance variable. It helps to write a lambda or
+     * method reference cleaner in that case.
      *
-     * @param set    - source set
-     * @param mapper - BiFunction mapper that describes how to map each element of the collection
-     * @param arg    - second argument for bi function
-     * @param <T>    - source set elements type
-     * @param <R>    - target set elements type
-     * @param <S>    - argument type
-     * @return mapped set
+     * @param set    The source collection.
+     * @param mapper The function to apply to each element.
+     * @param arg    The second argument that is used for {@link BiFunction}.
+     * @param <T>    The source collection elements type.
+     * @param <R>    The target set elements type.
+     * @param <S>    The second argument type.
+     * @return the new set that contains result of applying the function for elements of given set.
      */
     public static <T, R, S> Set<R> map(Set<T> set, BiFunction<? super T, ? super S, ? extends R> mapper, S arg) {
         return map(set, function(mapper, arg));
     }
 
     /**
-     * Map collection to a new collection according to the mapper
+     * Returns a collection consisting of the results of applying the given function to the elements of the
+     * given collection. Consumer the supplier for the new collection.
      *
-     * @param collection        - source collection
-     * @param mapper            - mapper that describes how to map each element of the collection
-     * @param collectionFactory - factory that describes how to create instance of the target collection
-     * @param <T>               - source collection elements type
-     * @param <R>               - target collection elements type
-     * @param <U>               - target collection type
-     * @return mapped collection of any the selected type
+     * @param collection        The source collection.
+     * @param mapper            The function to apply to each element.
+     * @param collectionFactory The new collection supplier.
+     * @param <T>               The source collection elements type.
+     * @param <R>               The target list elements type.
+     * @param <U>               The target collection elements type.
+     * @return the new list that contains result of applying the function for elements of given collection.
      */
     public static <T, R, U extends Collection<R>> U mapToCollection(Collection<T> collection,
                                                                     Function<? super T, ? extends R> mapper,
@@ -176,26 +191,20 @@ public final class CollectionUtils {
     }
 
     /**
-     * Map collection to a new array according to the mapper
+     * Returns an array consisting of the results of applying the given function to the elements of the given
+     * collection.
      *
-     * @param collection - source collection
-     * @param mapper     - mapper that describes how to map each element of the collection
-     * @param generator  - generator that describes how to create a new array of the target type
-     * @param <T>        - source collection elements type
-     * @param <R>        - target array elements type
-     * @return mapped array
+     * @param collection The source collection.
+     * @param mapper     The function to apply to each element.
+     * @param <T>        The source collection elements type.
+     * @param <R>        The target array elements type.
+     * @return the new array that contains result of applying the function for elements of given collection.
      */
     public static <T, R> R[] mapToArray(Collection<T> collection,
                                         Function<? super T, ? extends R> mapper,
                                         IntFunction<R[]> generator) {
         requireNonNull(collection, mapper, generator);
         return collectionToMappedStream(collection, mapper).toArray(generator);
-    }
-
-
-    private static <T, R> Stream<? extends R> collectionToMappedStream(Collection<T> collection,
-                                                                       Function<? super T, ? extends R> mapper) {
-        return collection.stream().map(mapper);
     }
 
     /**
@@ -322,6 +331,11 @@ public final class CollectionUtils {
         return (u, v) -> {
             throw new IllegalStateException(String.format("Duplicate key %s", u));
         };
+    }
+
+    private static <T, R> Stream<? extends R> collectionToMappedStream(Collection<T> collection,
+                                                                       Function<? super T, ? extends R> mapper) {
+        return collection.stream().map(mapper);
     }
 
 }
