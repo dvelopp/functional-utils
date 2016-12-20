489eed1e48265eedc9fc1ab40b8f11c02909fdc9
dvelopp <dvelopp@gmail.com>
2016-12-20 21:01:55 +0200

    ObjectUtilsTest optimization

--- src/test/java/com/dvelopp/functional/utils/ObjectUtilsTest.java
+++ src/test/java/com/dvelopp/functional/utils/ObjectUtilsTest.java
@@ -11,126 +11,101 @@ public class ObjectUtilsTest {
     @Rule public ExpectedException expectedException = ExpectedException.none();
 
     private final String TEST_EXCEPTION_MESSAGE = "message";
+    private final Object NOT_NULL_OBJECT = new Object();
+    private final Object NULL_OBJECT = null;
+
 
     @Test
     public void requireNonNull_TwoNotNullObjects_Success() {
-        Object o1 = new Object();
-        Object o2 = new Object();
-
-        requireNonNull(o1, o2);
+        requireNonNull(NOT_NULL_OBJECT, NOT_NULL_OBJECT);
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     public void requireNonNull_TwoNullObjects_NPEHasBeenThrown() {
-        Object o1 = null;
-        Object o2 = null;
         expectedException.expect(NullPointerException.class);
 
-        requireNonNull(o1, o2);
+        requireNonNull(NULL_OBJECT, NULL_OBJECT);
     }
 
     @Test
     public void requireNonNull_OneNullObjectAndOneNotNullObject_NPEHasBeenThrown() {
-        Object o1 = new Object();
-        Object o2 = null;
         expectedException.expect(NullPointerException.class);
 
-        requireNonNull(o1, o2);
+        requireNonNull(NOT_NULL_OBJECT, NULL_OBJECT);
     }
 
     @Test
     public void requireNonNull_OneNullObjectAndOneNotNullObjectReversed_NPEHasBeenThrown() {
-        Object o1 = null;
-        Object o2 = new Object();
         expectedException.expect(NullPointerException.class);
 
-        requireNonNull(o1, o2);
+        requireNonNull(NULL_OBJECT, NOT_NULL_OBJECT);
     }
 
     @Test
     public void requireNonNullWithMessage_TwoNotNullObjects_Success() {
-        Object o1 = new Object();
-        Object o2 = new Object();
-
-        requireNonNull(TEST_EXCEPTION_MESSAGE, o1, o2);
+        requireNonNull(TEST_EXCEPTION_MESSAGE, NOT_NULL_OBJECT, NOT_NULL_OBJECT);
     }
 
     @Test
     public void requireNonNullWithMessage_TwoNullObjects_NPEHasBeenThrownWithTestMessage() {
-        Object o1 = null;
-        Object o2 = null;
         expectedException.expect(NullPointerException.class);
         expectedException.expectMessage(TEST_EXCEPTION_MESSAGE);
 
-        requireNonNull(TEST_EXCEPTION_MESSAGE, o1, o2);
+        requireNonNull(TEST_EXCEPTION_MESSAGE, NULL_OBJECT, NULL_OBJECT);
     }
 
     @Test
     public void requireNonNullWithMessage_OneNullObjectAndOneNotNullObject_NPEHasBeenThrownWithTestMessage() {
-        Object o1 = new Object();
-        Object o2 = null;
         expectedException.expect(NullPointerException.class);
         expectedException.expectMessage(TEST_EXCEPTION_MESSAGE);
 
-        requireNonNull(TEST_EXCEPTION_MESSAGE, o1, o2);
+        requireNonNull(TEST_EXCEPTION_MESSAGE, NOT_NULL_OBJECT, NULL_OBJECT);
     }
 
     @Test
     public void requireNonNullWithMessage_OneNullObjectAndOneNotNullObjectReversed_NPEHasBeenThrownWithTestMessage() {
-        Object o1 = null;
-        Object o2 = new Object();
         expectedException.expect(NullPointerException.class);
         expectedException.expectMessage(TEST_EXCEPTION_MESSAGE);
 
-        requireNonNull(TEST_EXCEPTION_MESSAGE, o1, o2);
+        requireNonNull(TEST_EXCEPTION_MESSAGE, NOT_NULL_OBJECT, NULL_OBJECT);
     }
 
     @Test
     public void requireNonNullWithMessageSupplier_TwoNotNullObjects_Success() {
-        Object o1 = new Object();
-        Object o2 = new Object();
-
-        requireNonNull(() -> TEST_EXCEPTION_MESSAGE, o1, o2);
+        requireNonNull(() -> TEST_EXCEPTION_MESSAGE, NOT_NULL_OBJECT, NOT_NULL_OBJECT);
     }
 
     @Test
     public void requireNonNullWithMessageSupplier_TwoNullObjects_NPEHasBeenThrownWithTestMessage() {
-        Object o1 = null;
-        Object o2 = null;
         expectedException.expect(NullPointerException.class);
         expectedException.expectMessage(TEST_EXCEPTION_MESSAGE);
 
-        requireNonNull(() -> TEST_EXCEPTION_MESSAGE, o1, o2);
+        requireNonNull(() -> TEST_EXCEPTION_MESSAGE, NULL_OBJECT, NULL_OBJECT);
     }
 
     @Test
     public void requireNonNullWithMessageSupplier_OneNullObjectAndOneNotNullObject_NPEHasBeenThrownWithTestMessage() {
-        Object o1 = new Object();
-        Object o2 = null;
         expectedException.expect(NullPointerException.class);
         expectedException.expectMessage(TEST_EXCEPTION_MESSAGE);
 
-        requireNonNull(() -> TEST_EXCEPTION_MESSAGE, o1, o2);
+        requireNonNull(() -> TEST_EXCEPTION_MESSAGE, NOT_NULL_OBJECT, NULL_OBJECT);
     }
 
     @Test
     public void requireNonNullWithMessageSupplier_OneNullObjectAndOneNotNullObjectReversed_NPEHasBeenThrownWithTestMessage() {
-        Object o1 = null;
-        Object o2 = new Object();
         expectedException.expect(NullPointerException.class);
         expectedException.expectMessage(TEST_EXCEPTION_MESSAGE);
 
-        requireNonNull(() -> TEST_EXCEPTION_MESSAGE, o1, o2);
+        requireNonNull(() -> TEST_EXCEPTION_MESSAGE, NULL_OBJECT, NOT_NULL_OBJECT);
     }
 
     @Test
     public void requireNonNullWithMessageSupplier_IllegalArgumentExceptionInSupplier_IllegalArgumentExceptionHasBeenThrown() {
-        Object o1 = null;
-        Object o2 = new Object();
         expectedException.expect(IllegalArgumentException.class);
 
         requireNonNull(() -> {
             throw new IllegalArgumentException();
-        }, o1, o2);
+        }, NULL_OBJECT, NOT_NULL_OBJECT);
     }
 }
