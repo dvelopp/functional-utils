c26da5ef3bc5d8c173c1acf16e6357129530a072
dvelopp <dvelopp@gmail.com>
2016-12-22 22:15:41 +0200

    ObjectUtils. Var args are used for not null checks only. Make them safe. They have no chance to fail.

--- src/main/java/com/dvelopp/functional/utils/ObjectUtils.java
+++ src/main/java/com/dvelopp/functional/utils/ObjectUtils.java
@@ -6,16 +6,19 @@ import java.util.stream.Stream;
 
 public class ObjectUtils {
 
+    @SafeVarargs
     public static <T> T[] requireNonNull(T... objectsToCheck) {
         Stream.of(objectsToCheck).forEach(Objects::requireNonNull);
         return objectsToCheck;
     }
 
+    @SafeVarargs
     public static <T> T[] requireNonNull(String message, T... objectsToCheck) {
         Stream.of(objectsToCheck).forEach(o -> Objects.requireNonNull(o, message));
         return objectsToCheck;
     }
 
+    @SafeVarargs
     public static <T> T[] requireNonNull(Supplier<String> messageSupplier, T... objectsToCheck) {
         Stream.of(objectsToCheck).forEach(o -> Objects.requireNonNull(o, messageSupplier));
         return objectsToCheck;
