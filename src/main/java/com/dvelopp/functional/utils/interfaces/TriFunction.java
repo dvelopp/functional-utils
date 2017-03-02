2f5403ebf371420a8faedca099fe9a3044106324
dvelopp <dvelopp@gmail.com>
2017-03-02 23:50:11 +0200

    TriFunction

---
+++ src/main/java/com/dvelopp/functional/utils/interfaces/TriFunction.java
@@ -0,0 +1,15 @@
+package com.dvelopp.functional.utils.interfaces;
+
+public interface TriFunction<T1, T2, T3, R> {
+
+
+    /**
+     * Applies this function to the given arguments.
+     *
+     * @param t1 the first input argument.
+     * @param t2 the second input argument.
+     * @param t3 the third input argument.
+     */
+    R apply(T1 t1, T2 t2, T3 t3);
+    
+}
