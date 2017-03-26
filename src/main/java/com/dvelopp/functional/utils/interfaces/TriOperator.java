c503f4790c42e6bd5e853054935169e88bba77e3
dvelopp <dvelopp@gmail.com>
2017-03-26 22:31:50 +0300

    TriOperator documentation

--- src/main/java/com/dvelopp/functional/utils/interfaces/TriOperator.java
+++ src/main/java/com/dvelopp/functional/utils/interfaces/TriOperator.java
@@ -1,5 +1,12 @@
 package com.dvelopp.functional.utils.interfaces;
 
+/**
+ * Represents an operation upon three operands of the same type, producing a result
+ * of the same type as the operands.  This is a specialization of
+ * {@link TriFunction} for the case where the operands and the result are all of
+ * the same type.
+ * @param <T>
+ */
 @FunctionalInterface
 public interface TriOperator<T> extends TriFunction<T,T,T,T> {
 
