c490e0eb7ec0928d66bf06853f5012c02c18fb2e
dvelopp <dvelopp@gmail.com>
2017-04-21 00:04:21 +0300

    added javadoc

--- src/main/java/com/dvelopp/functional/utils/interfaces/TriPredicate.java
+++ src/main/java/com/dvelopp/functional/utils/interfaces/TriPredicate.java
@@ -1,6 +1,14 @@
 package com.dvelopp.functional.utils.interfaces;
 
+import java.util.function.Predicate;
+
 /**
+ * Represents a predicate (boolean-valued function) of three arguments.  This is
+ * the three-arity specialization of {@link Predicate}.
+ * <p>
+ * <p>This is a <a href="package-summary.html">functional interface</a>
+ * whose functional method is {@link #test(Object, Object, Object)}.
+ *
  * @param <T1> The type of the first argument to the predicate.
  * @param <T2> The type of the second argument to the predicate.
  * @param <T3> The type of the third argument to the predicate.
