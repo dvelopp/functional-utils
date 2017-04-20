acb593e776d08a837e9f498893181157ecdd1aae
dvelopp <dvelopp@gmail.com>
2017-04-20 23:58:45 +0300

    added javadoc

--- src/main/java/com/dvelopp/functional/utils/interfaces/TriFunction.java
+++ src/main/java/com/dvelopp/functional/utils/interfaces/TriFunction.java
@@ -1,6 +1,11 @@
 package com.dvelopp.functional.utils.interfaces;
 
+import java.util.function.Function;
+
 /**
+ * Represents a function that accepts three arguments and produces a result.
+ * This is the three-arity specialization of {@link Function}.
+ *
  * @param <T1> The type of the first argument to the function.
  * @param <T2> The type of the second argument to the function.
  * @param <T3> The type of the third argument to the function.
